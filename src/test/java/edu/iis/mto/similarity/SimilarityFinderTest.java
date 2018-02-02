47c1b9e9cae4274cf47e0487b0bde14a0ca362ef
oktawianmankowski <198162@edu.p.lodz.pl>
2018-02-03 00:35:50 +0100

    SimilarityFinder Test created

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,66 @@
+package edu.iis.mto.similarity;
+
+import org.junit.Test;
+
+public class SimilarityFinderTest {
+
+    @Test
+    public void calculateJackardSimilarityBothSequencesAreEmpty() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilaritySecondSequencesIsEmpty() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilaritySizeIsEqualToOneAndHaveOneCommonElement() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilaritySizeIsEqualToOneAndHaveNoCommonElements() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityDifferentSequenceSizesNoCommonElements() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityDifferentSequenceSizesOneCommonElement() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityDifferentSequenceSizesAllElementsAreCommon() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityDifferentSequenceSizesHalfOfElementsAreCommon() {
+        ;
+    }
+
+    public void calculateJackardSimilarityTheSameSequenceSizesNoCommonElements() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityTheSameSequenceSizesOneCommonElement() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityTheSameSequenceSizesAllElementsAreCommon() {
+        ;
+    }
+
+    @Test
+    public void calculateJackardSimilarityTheSameSequenceSizesHalfOfElementsAreCommon() {
+        ;
+    }
+
+}
