775ef8c6060cf88871ee408f11b3c2e5a298f468
oktawianmankowski <198162@edu.p.lodz.pl>
2018-02-03 01:32:15 +0100

    tests - different sequence sizes

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -67,25 +67,58 @@ public class SimilarityFinderTest {
 
     @Test
     public void calculateJackardSimilarityDifferentSequenceSizesNoCommonElements() {
-        ;
+
+        int[] fisrtSequence = {1, 2, 3};
+        int[] secondSequence = {4, 5, 6, 7, 8, 9};
+        SequenceSearcherDubler.searchResults.push(false);
+        SequenceSearcherDubler.searchResults.push(false);
+        SequenceSearcherDubler.searchResults.push(false);
+        SimilarityFinder objectUnderTest = new SimilarityFinder(new SequenceSearcherDubler());
+        double expectedJackardSimilarity = 0.;
+        double actualJackardSimilarity = objectUnderTest.calculateJackardSimilarity(fisrtSequence, secondSequence);
+        double delta = Math.abs(actualJackardSimilarity - expectedJackardSimilarity);
+        boolean actualOutput = delta < EPSILON;
+
+        assertThat(EXPECTED, Matchers.equalTo(actualOutput));
     }
 
     @Test
     public void calculateJackardSimilarityDifferentSequenceSizesOneCommonElement() {
-        ;
+
+        int[] fisrtSequence = {1, 2};
+        int[] secondSequence = {2, 3, 4};
+        SequenceSearcherDubler.searchResults.push(true);
+        SequenceSearcherDubler.searchResults.push(false);
+        SimilarityFinder objectUnderTest = new SimilarityFinder(new SequenceSearcherDubler());
+        double expectedJackardSimilarity = 0.25;
+        double actualJackardSimilarity = objectUnderTest.calculateJackardSimilarity(fisrtSequence, secondSequence);
+        double delta = Math.abs(actualJackardSimilarity - expectedJackardSimilarity);
+        boolean actualOutput = delta < EPSILON;
+
+        assertThat(EXPECTED, Matchers.equalTo(actualOutput));
     }
 
     @Test
-    public void calculateJackardSimilarityDifferentSequenceSizesAllElementsAreCommon() {
-        ;
+    public void calculateJackardSimilarityDifferentSequenceSizesSomeElementsAreCommon() {
+
+        int[] fisrtSequence = {1, 2, 3, 4, 5, 6};
+        int[] secondSequence = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        SequenceSearcherDubler.searchResults.push(true);
+        SequenceSearcherDubler.searchResults.push(true);
+        SequenceSearcherDubler.searchResults.push(true);
+        SequenceSearcherDubler.searchResults.push(false);
+        SequenceSearcherDubler.searchResults.push(false);
+        SequenceSearcherDubler.searchResults.push(false);
+        SimilarityFinder objectUnderTest = new SimilarityFinder(new SequenceSearcherDubler());
+        double expectedJackardSimilarity = 0.25;
+        double actualJackardSimilarity = objectUnderTest.calculateJackardSimilarity(fisrtSequence, secondSequence);
+        double delta = Math.abs(actualJackardSimilarity - expectedJackardSimilarity);
+        boolean actualOutput = delta < EPSILON;
+
+        assertThat(EXPECTED, Matchers.equalTo(actualOutput));
     }
 
-    @Test
-    public void calculateJackardSimilarityDifferentSequenceSizesHalfOfElementsAreCommon() {
-        ;
-    }
-
-    public void calculateJackardSimilarityTheSameSequenceSizesNoCommonElements() {
+     public void calculateJackardSimilarityTheSameSequenceSizesNoCommonElements() {
         ;
     }
 
